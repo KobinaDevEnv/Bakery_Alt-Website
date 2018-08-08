48397cdabdb28267a1d690fecff3dcf94610fb3a
KobinaDevEnv <41956968+KobinaDevEnv@users.noreply.github.com>
2018-08-08 13:32:23 -0400

    firebase files

--- public/app.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyD-cYIenbTsV25bYumiPigZCH2pANdwdIw",
-    authDomain: "learned-iris-212616.firebaseapp.com",
-    databaseURL: "https://learned-iris-212616.firebaseio.com",
-    projectId: "learned-iris-212616",
-    storageBucket: "",
-    messagingSenderId: "216675282902"
-  };
-  firebase.initializeApp(config);
